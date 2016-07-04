337f09779fdad17f0495c73c3e62f4ab6ea61b6e
hzhenhan <zhenhan@Zhenhans.local>
2016-07-04 16:28:25 -0400

    ProgrammingAssignment2_Zhenhan_Huang
    ProgrammingAssignment2_Zhenhan_Huang

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,34 @@
 ## Put comments here that give an overall description of what your
 ## functions do
+## makeVector stores a matrix (x) in the memory
+## cachemean gives inverse of the matrix if it is in the memory or computes the inverse and then shows the inverse
 
 ## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+## makeVector uses scoping rules and stores matrices in memory
+makeVector <- function(x = numeric()) {
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setmean <- function(mean) m <<- mean
+  getmean <- function() m
+  list(set = set, get = get,
+       setmean = setmean,
+       getmean = getmean)
 }
 
-
 ## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+## the input x must be a makeVector
+cachemean <- function(x, ...) {
+  m <- x$getmean()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- mean(data, ...)
+  x$setmean(m)
+  m
 }
